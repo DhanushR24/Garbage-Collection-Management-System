20ea75e87bce096e6acc11a242bb655ec8db2d31
Dhanush Raj <dhanushra66@gmail.com>
2022-09-20 15:02:01 +0530

    support for mongoDB #1 🎨

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 
 const port = process.env.PORT || 3000;
 
+const { checkUser } = require("./helpers/helper");
+const userRouter = require("./routers/UserRouter");
+
 const app = express();
 app.use(morgan("dev"));
 app.use(express.static("public"));
@@ -15,17 +18,29 @@ app.use(
   })
 );
 
+app.use(userRouter);
+
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-mongoose.connect(
-  "mongodb+srv://DbConnect:test12345@cluster.bh7j7.mongodb.net/GarbageCollection",
-  () => {
+mongoose
+  .connect(
+    "mongodb+srv://DbConnect:test12345@cluster.bh7j7.mongodb.net/GarbageCollection?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then((res) => {
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
-  }
-);
+  })
+  .catch((err) => console.log(err));
+
+app.get("*", checkUser);
+
+app.get("/", (req, res) => {
+  res.render("main", {
+    title: "Home",
+  });
+});
