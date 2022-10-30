3d598ed5412a2e2b0092cb5ed30344cc05542222
Dhanush Raj <dhanushra66@gmail.com>
2022-10-30 12:07:22 +0530

    log out feature added

--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -7,5 +7,6 @@ router.get("/signup", userController.signup_get);
 router.post("/signup", userController.signup_post);
 router.get("/login", userController.login_get);
 router.post("/login", userController.login_post);
+router.get("/logout", userController.logout_get);
 
 module.exports = router;
