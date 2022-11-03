05b7baa9a32b591114ae6c356f300c48f0539714
Dhanush Raj <dhanushra66@gmail.com>
2022-11-03 06:09:20 +0530

    User preference (pickup) updation feature :construction:

---
+++ controllers/collectController.js
@@ -0,0 +1,33 @@
+const User = require("../models/user");
+
+module.exports.pickup_post = (req, res) => {
+  const id = req.params.id;
+
+  User.findById(id)
+    .then((result) => {
+      const pickup = !result.pickup;
+
+      User.findByIdAndUpdate(id, { pickup })
+        .then((result) => {
+          res.json({ redirect: "/" });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports.track = (req, res) => {
+  res.render("track", {
+    title: "Track",
+  });
+};
+
+module.exports.pickup_get = (req, res) => {
+  res.render("preference", {
+    title: "Preference",
+  });
+};
--- models/User.js
+++ models/User.js
@@ -30,6 +30,10 @@ const userSchema = new Schema({
     type: Boolean,
     default: false,
   },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const User = mongoose.model("User", userSchema);
